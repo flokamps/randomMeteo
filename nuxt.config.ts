b7e95a661c14f9d9e19fc634a3b866c92f6e93b4
Florian Kamps <florian@studioeleven.cc>
2024-03-04 15:21:18 +0100

    base nuxt app

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  components: [
+    {
+        path: '~/components',
+        pathPrefix: true
+    }
+  ]
+})
