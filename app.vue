18aa2e7fd09b5d1b91bd31d7699c791971c48344
Florian Kamps <florian@studioeleven.cc>
2024-03-04 15:32:57 +0100

    check if mobile or not

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
+  <div v-if="isMobile">
     <Test />
   </div>
+  <div v-else>
+    <MobileWarning />
+  </div>
 </template>
+<script setup lang="ts">
+const isMobile = ref(false);
+const checkIfMobile = () => {
+  isMobile.value = window.innerWidth < 768;
+};
+
+onMounted(() => {
+  checkIfMobile();
+  window.addEventListener('resize', checkIfMobile);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', checkIfMobile);
+});
+</script>
