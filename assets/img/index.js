a05d96f9eb62cadb151b6512bef0ecf81b34507f
Florian Kamps <florian@studioeleven.cc>
2024-03-05 10:11:45 +0100

    transitions

--- assets/img/index.js
+++ assets/img/index.js
@@ -2,10 +2,14 @@ import bolt from './bolt.svg';
 import clouds from './clouds.svg';
 import rain from './rain.svg';
 import snow from './snow.svg';
+import sunnyCloudy from './sunnyCloudy.svg';
+import sun from './sun.svg';
 
 export const icons = {
     clouds,
     rain,
     bolt,
     snow,
+    sunnyCloudy,
+    sun
 };
--- components/WeatherScreen.vue
+++ components/WeatherScreen.vue
@@ -1,25 +1,33 @@
 <template>
   <div class="flex flex-col pl-5 h-screen">
     <div id="weatherIcon" class="flex justify-end relative overflow-hidden">
-      <img class="absolute" :src="props.weather.cover" alt="Sunny and Cloudy" />
+      <transition name="cover" mode="in-out">
+        <img class="absolute cover-enter-active" :src="props.weather.cover" alt="Weather cover" />
+      </transition>
     </div>
-    <div id="temperature">
-      <p class="font-bold">{{ Math.round(props.temperature) }}°</p>
-    </div>
-    <div class="flex" id="weatherDescription">
-      <img :src="props.weather.icon" alt="Rain" />
-      <p class="ml-1 font-light">{{ props.weather.credo }}</p>
-    </div>
-    <div id="city">
-      <p class="font-bold">{{ props.city }}</p>
-    </div>
-    <div id="cityDescription">
-      <p class="font-light">Montluçon est une commune française située dans le centre de la France, sous-préfecture du département de l'Allier dans la région Auvergne-Rhône-Alpes...</p>
-    </div>
-    <div id="swipe" class="flex flex-col font-bold items-center mt-auto mb-6">
-      <p>Glisser pour voyager</p>
-      <img :src="swipe" />
-    </div>
+    <transition name="texts" mode="in-out">
+      <div id="texts" class="flex-col flex texts-enter-active">
+        <div id="temperature">
+          <p class="font-bold">{{ Math.round(props.temperature) }}°</p>
+        </div>
+        <div class="flex items-center" id="weatherDescription">
+          <img :src="props.weather.icon" alt="Rain" />
+          <p class="ml-1 font-light">{{ props.weather.credo }}</p>
+        </div>
+        <div id="city">
+          <p class="font-bold">{{ props.city }}</p>
+        </div>
+        <div id="cityDescription">
+          <p class="font-light">Montluçon est une commune française située dans le centre de la France, sous-préfecture du département de l'Allier dans la région Auvergne-Rhône-Alpes...</p>
+        </div>
+      </div>
+    </transition>
+    <transition name="swipe" mode="in-out">
+      <div id="swipe" class="flex flex-col font-bold items-center mt-auto mb-6 swipe-enter-active">
+        <p>Glisser pour voyager</p>
+        <img :src="swipe"  alt="Swipe icon"/>
+      </div>
+    </transition>
   </div>
 </template>
 <script setup>
