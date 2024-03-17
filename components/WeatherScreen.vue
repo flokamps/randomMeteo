21d390bac5c629e23c73048788382740f20bde68
Florian Kamps <florian@studioeleven.cc>
2024-03-17 22:50:08 +0100

    remove console.log oopsie

--- components/WeatherScreen.vue
+++ components/WeatherScreen.vue
@@ -49,8 +49,6 @@ const handleScroll = () => {
 
   const scrollDistance = mainDiv.value.scrollTop;
 
-  console.log(windowHeight, scrollDistance);
-
   if (scrollDistance >= windowHeight) {
     emit('reload');
   }
