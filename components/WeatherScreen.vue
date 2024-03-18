1b016da6f520676143d72ae304ec87f6b06dc2ad
Florian Kamps <florian@studioeleven.cc>
2024-03-18 15:35:51 +0100

    change text

--- components/WeatherScreen.vue
+++ components/WeatherScreen.vue
@@ -4,18 +4,10 @@
       <img ref="settingsImage" :src="settings" alt="Settings"/>
     </div>
     <div ref="settingsInterface"
-         class="rounded-3xl	absolute hidden top-10 left-5 w-9/10 h-9/10 z-10 settings-interface-active flex flex-col items-center"
+         class="rounded-3xl	absolute hidden top-10 left-5 w-9/10 h-9/10 z-10 settings-interface-active flex flex-col-reverse items-center"
          id="settingsInterface">
       <div class="flex-auto flex flex-col justify-evenly">
-        <p class="mx-5 text-lg">Cette application web est inspiré de la maquette Figma du Youtubeur BastiUI et réalisé par Florian Kamps. Elle a entièrement été réalisé en Vue.JS avec Nuxt.JS et TailwindCSS. Elle utilise l'API de Meteo France et Wikipedia</p>
-        <div class="persona-link">
-          <h5 class="text-3xl font-bold">Florian Kamps</h5>
-          <div class="socials py-5">
-            <a href="https://www.linkedin.com/in/florian-kamps/"><img :src="socials.linkedin" alt="Linkedin"></a>
-            <a href="https://github.com/flokamps"><img :src="socials.github" alt="Github"></a>
-            <a href="https://twitter.com/rootmeih"><img :src="socials.twitter" alt="Twitter"></a>
-          </div>
-        </div>
+        <p class="mx-5 text-xs">Inspiré par BastUI, développé par Florian Kamps</p>
         <div class="persona-link">
           <h5 class="text-3xl font-bold">BastiUI</h5>
           <div class="socials py-5">
@@ -23,6 +15,14 @@
             <a href="https://www.linkedin.com/in/bastienmarecaux/"><img :src="socials.linkedin" alt="Linkedin"></a>
             <a href="https://www.figma.com/@bastiui"><img :src="socials.figma" alt="Figma"></a>
             <a href="https://www.twitch.tv/bastiui"><img :src="socials.twitch" alt="Twitch"></a>
+          </div>
+        </div>
+        <div class="persona-link">
+          <h5 class="text-3xl font-bold">Florian Kamps</h5>
+          <div class="socials py-5">
+            <a href="https://www.linkedin.com/in/florian-kamps/"><img :src="socials.linkedin" alt="Linkedin"></a>
+            <a href="https://github.com/flokamps"><img :src="socials.github" alt="Github"></a>
+            <a href="https://twitter.com/rootmeih"><img :src="socials.twitter" alt="Twitter"></a>
           </div>
         </div>
       </div>
